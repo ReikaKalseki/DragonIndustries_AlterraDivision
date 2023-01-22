003aca7ae78b4e61bc9b3bbd4405c0a266b6680d
Reika <reikasminecraft@gmail.com>
2023-01-21 19:52:44 -0500

    Update 122

--- Base/Biome.cs
+++ Base/Biome.cs
@@ -18,7 +18,9 @@ namespace ReikaKalseki.DIAlterra
 		
 		public readonly string biomeName;
 		
-		private static float lastMusicControlTime = -1;
+		private static float nextMusicChoiceTime = -1;
+		private static VanillaMusic currentMusic = null;
+		private static Biome currentBiome = null;
 		
 		protected Biome(string name) {
 			biomeName = name;
@@ -32,40 +34,62 @@ namespace ReikaKalseki.DIAlterra
 			
 		}
 		
+		public virtual float getNextMusicSilencePadding() {
+			return 0;
+		}
+		
 		public abstract VanillaMusic[] getMusicOptions();
 		public abstract Vector3 getFogColor(Vector3 orig);
 		public abstract float getSunIntensity(float orig);
 		public abstract float getFogDensity(float orig);
 		
 		public static Biome getBiome(string name) {
-			return biomeList.ContainsKey(name) ? biomeList[name] : null;
+			return !string.IsNullOrEmpty(name) && biomeList.ContainsKey(name) ? biomeList[name] : null;
 		}
 		
 		public static void tickMusic(DayNightCycle cyc) {
-			float dur = cyc.timePassedAsFloat-lastMusicControlTime;
-			if (dur >= 1) {
-				lastMusicControlTime = cyc.timePassedAsFloat;
+			if (cyc.timePassedAsFloat >= nextMusicChoiceTime) {
 				Player ep = Player.main;
 				if (ep) {
 					Vector3 pos = ep.transform.position;
 					string biome = WaterBiomeManager.main.GetBiome(pos);
 					Biome b = getBiome(biome);
+					bool changed = b != currentBiome;
+					currentBiome = b;
+					if (changed) {
+						if (currentMusic != null)
+							currentMusic.stop();
+						nextMusicChoiceTime = cyc.timePassedAsFloat+1;
+						return;
+					}
 					if (b != null) {
 						VanillaMusic[] mus = b.getMusicOptions();
+						//SNUtil.writeToChat(b.biomeName+" > "+(mus != null ? string.Join(",", (object[])mus) : "null"));
 						if (mus != null) {
 							foreach (VanillaMusic vm in VanillaMusic.getAll()) {
-								vm.disable();
+								vm.stop();
 							}
-							foreach (VanillaMusic vm in mus) {
-								vm.enable();
-								vm.setToBiome(biome);
+							if (mus.Length > 0) {/*
+								foreach (VanillaMusic vm in VanillaMusic.getAll()) {
+									vm.disable();
+								}
+								foreach (VanillaMusic vm in mus) {
+									vm.enable();
+									vm.setToBiome(biome);
+								}*/
+								VanillaMusic track = mus[UnityEngine.Random.Range(0, mus.Length)];
+								track.play();
+								nextMusicChoiceTime = cyc.timePassedAsFloat+track.getLength()+b.getNextMusicSilencePadding();
+								currentMusic = track;
+								//SNUtil.writeToChat("Selected play of track "+track+" @ "+cyc.timePassedAsFloat+" > "+nextMusicChoiceTime);
 							}
 						}
 					}
-					else {
+					else {/*
 						foreach (VanillaMusic vm in VanillaMusic.getAll()) {
 							vm.reset();
-						}
+						}*/
+						nextMusicChoiceTime = cyc.timePassedAsFloat+1;
 					}
 				}
 			}
--- Registry/VanillaData/VanillaMusic.cs
+++ Registry/VanillaData/VanillaMusic.cs
@@ -17,41 +17,45 @@ namespace ReikaKalseki.DIAlterra
 		private static readonly Dictionary<string, VanillaMusic> table = new Dictionary<string, VanillaMusic>();
 		
 		//Where does Crush Depth play?
-		public static readonly VanillaMusic KOOSH = new VanillaMusic("kooshAmbience"); //Islands Beneath The Sea
-		public static readonly VanillaMusic BKELP = new VanillaMusic("BloodKelpAmbience"); //Lava Castle
-		public static readonly VanillaMusic DUNES = new VanillaMusic("dunes"); //Alien Expanse
-		public static readonly VanillaMusic GRANDREEF = new VanillaMusic("grandReefAmbience"); //Lost River
-		public static readonly VanillaMusic DEEPGRAND = new VanillaMusic("deepGrandReefAmbience"); //Blood Crawlers
-		public static readonly VanillaMusic UNDERISLANDS = new VanillaMusic("UnderwaterIslandsAmbience"); //silent?
-		public static readonly VanillaMusic SHALLOWS = new VanillaMusic("safeShallowsAmbience"); //Into The Unknown
-		public static readonly VanillaMusic KELP = new VanillaMusic("kelpForestAmbience"); //In Bloom
-		public static readonly VanillaMusic OBSERVATORY = new VanillaMusic("observatoryAmbience"); //Observatory Zen
-		public static readonly VanillaMusic MOUNTAINS = new VanillaMusic("mountainsAmbience"); //Fear The Reapers
-		public static readonly VanillaMusic MUSHROOM = new VanillaMusic("mushroomForestAmbience"); //?
-		public static readonly VanillaMusic REDGRASS = new VanillaMusic("grassyPlateausAmbience"); //?
-		public static readonly VanillaMusic LOSTRIVER = new VanillaMusic("lostRiverAmbience"); //Lost River
-		public static readonly VanillaMusic JELLYSHROOM = new VanillaMusic("jellyshroomCaveAmbience"); //Crash Site
-		public static readonly VanillaMusic FLOATINGISLAND = new VanillaMusic("floatingIslandAmbience"); //?
-		public static readonly VanillaMusic LAVAZONE = new VanillaMusic("lavaZoneAmbience"); //Lava Castle
-		public static readonly VanillaMusic ILZ = new VanillaMusic("inactiveLavaZone"); //Bring a Medpack
-		public static readonly VanillaMusic ALZ = new VanillaMusic("alz"); //Ahead Slow
-		public static readonly VanillaMusic AURORA = new VanillaMusic("crashedShipAmbience"); //Dark Matter Reactor
-		public static readonly VanillaMusic TREADER = new VanillaMusic("seaTreaderPath"); //Islands Beneath The Sea
-		public static readonly VanillaMusic COVE = new VanillaMusic("LostRiverTreeCove"); //Ghost Tree
-		public static readonly VanillaMusic COVETREE = new VanillaMusic("LostRiverGhostTree"); //silent?
-		public static readonly VanillaMusic SPARSE = new VanillaMusic("SparseReefAmbience"); //Islands Beneath The Sea
-		public static readonly VanillaMusic CRASH = new VanillaMusic("crashZoneAmbience"); //?
-		public static readonly VanillaMusic WRECK = new VanillaMusic("WreckAmbience"); //not a music track, just the wreckage near pod 6
-		public static readonly VanillaMusic GENERATOR = new VanillaMusic("generatorRoomAmbience"); //Bring a Medpack
-		public static readonly VanillaMusic SCANNER = new VanillaMusic("mapRoomAmbience"); //Scanner Room Ambient
+		public static readonly VanillaMusic KOOSH = new VanillaMusic("kooshAmbience", 2, 48); //Islands Beneath The Sea
+		public static readonly VanillaMusic BKELP = new VanillaMusic("BloodKelpAmbience", 2, 12); //Lava Castle
+		public static readonly VanillaMusic DUNES = new VanillaMusic("dunes", 1, 41); //Alien Expanse
+		public static readonly VanillaMusic GRANDREEF = new VanillaMusic("grandReefAmbience", 1, 22); //Lost River
+		public static readonly VanillaMusic DEEPGRAND = new VanillaMusic("deepGrandReefAmbience", 1, 44); //Blood Crawlers
+		public static readonly VanillaMusic UNDERISLANDS = new VanillaMusic("UnderwaterIslandsAmbience", 0, 0); //silent?
+		public static readonly VanillaMusic SHALLOWS = new VanillaMusic("safeShallowsAmbience", 2, 59); //Into The Unknown
+		public static readonly VanillaMusic KELP = new VanillaMusic("kelpForestAmbience", 2, 10); //In Bloom
+		public static readonly VanillaMusic OBSERVATORY = new VanillaMusic("observatoryAmbience", 1, 23); //Observatory Zen
+		public static readonly VanillaMusic MOUNTAINS = new VanillaMusic("mountainsAmbience", 1, 23); //Fear The Reapers
+		public static readonly VanillaMusic MUSHROOM = new VanillaMusic("mushroomForestAmbience", 0, 0); //?
+		public static readonly VanillaMusic REDGRASS = new VanillaMusic("grassyPlateausAmbience", 0, 0); //?
+		public static readonly VanillaMusic LOSTRIVER = new VanillaMusic("lostRiverAmbience", 1, 22); //Lost River
+		public static readonly VanillaMusic JELLYSHROOM = new VanillaMusic("jellyshroomCaveAmbience", 2, 30); //Crash Site
+		public static readonly VanillaMusic FLOATINGISLAND = new VanillaMusic("floatingIslandAmbience", 0, 0); //?
+		public static readonly VanillaMusic LAVAZONE = new VanillaMusic("lavaZoneAmbience", 2, 12); //Lava Castle
+		public static readonly VanillaMusic ILZ = new VanillaMusic("inactiveLavaZone", 1, 57); //Bring a Medpack
+		public static readonly VanillaMusic ALZ = new VanillaMusic("alz", 1, 40); //Ahead Slow
+		public static readonly VanillaMusic AURORA = new VanillaMusic("crashedShipAmbience", 1, 31); //Dark Matter Reactor
+		public static readonly VanillaMusic TREADER = new VanillaMusic("seaTreaderPath", 2, 48); //Islands Beneath The Sea
+		public static readonly VanillaMusic COVE = new VanillaMusic("LostRiverTreeCove", 1, 41); //Ghost Tree
+		public static readonly VanillaMusic COVETREE = new VanillaMusic("LostRiverGhostTree", 0, 0); //silent?
+		public static readonly VanillaMusic SPARSE = new VanillaMusic("SparseReefAmbience", 2, 48); //Islands Beneath The Sea
+		public static readonly VanillaMusic CRASH = new VanillaMusic("crashZoneAmbience", 0, 0); //?
+		public static readonly VanillaMusic WRECK = new VanillaMusic("WreckAmbience", 0, 0); //not a music track, just the wreckage near pod 6
+		public static readonly VanillaMusic GENERATOR = new VanillaMusic("generatorRoomAmbience", 1, 57); //Bring a Medpack
+		public static readonly VanillaMusic SCANNER = new VanillaMusic("mapRoomAmbience", 0, 0); //Scanner Room Ambient
 		
 		private readonly string objectName;
 		
 		private string activationBiome;
+		
+		private float length;
 				
-		private VanillaMusic(string id) {
+		private VanillaMusic(string id, int min, int sec) {
 			objectName = id;
 			table[objectName] = this;
+			
+			length = min*60+sec;
 		}
 		
 		public override string ToString() {
@@ -69,8 +73,22 @@ namespace ReikaKalseki.DIAlterra
 				go.GetComponent<FMODGameParams>().onlyInBiome = activationBiome;
 		}
 		
+		public void play() {
+			;//getObject().GetComponent<FMOD_CustomLoopingEmitter>().Play();
+			//SoundManager.playSound(getObject().GetComponent<FMOD_CustomLoopingEmitter>().asset.path);
+			FMODUWE.PlayOneShot(getObject().GetComponent<FMOD_CustomLoopingEmitter>().asset, Player.main.transform.position, 1);
+		}
+		
 		public void enable() {
 			getObject().SetActive(true);
+		}
+		
+		public void stop() {
+			//getObject().GetComponent<FMOD_CustomLoopingEmitter>().Stop();
+		}
+		
+		public float getLength() {
+			return length;//getObject().GetComponent<FMOD_CustomLoopingEmitter>().length;
 		}
 		
 		public void disable() {
